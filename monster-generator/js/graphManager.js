1b6646ee58dc3b055f22809ee6a5f6f5cd72bfec
Gabrielle Bennett <gmb9280@rit.edu>
2015-02-15 17:01:36 -0500

    Added increment / decrement graph functions... Working test version on the HP graph. Check it out.

--- monster-generator/js/graphManager.js
+++ monster-generator/js/graphManager.js
@@ -11,6 +11,8 @@ var graphids =
 	{'id': 'wisGraph', 'graph': {}}
 ];
 
+// Helper function: Takes string of 
+// Graph ID and returns the graph object associated. 
 function getGraphObj(graphID)
 {
 	for(var i =0;i< graphids.length; i++)
@@ -18,6 +20,45 @@ function getGraphObj(graphID)
 		if(graphids[i].id == graphID)
 			return graphids[i].graph;
 		else continue; 
+	}
+}
+
+// Increment graph as a matter of scale. 
+function incrementGraph(graphID)
+{
+	// easy access to graph obj
+	var graph = getGraphObj(graphID); 
+	if(graph == undefined){ return; }
+
+	console.log(graph);
+	console.log(graph.datasets);
+	for(var i = 0; i < graph.datasets[0].points.length; i++)
+	{
+		graph.datasets[0].points[i].value ++;
+	}
+	graph.update();
+}
+
+function decrementGraph(graphID)
+{
+// easy access to graph obj
+	var graph = getGraphObj(graphID); 
+	if(graphID == undefined) return; 
+
+	for(var i = 0; i < graph.datasets[0].points.length; i++)
+	{
+		graph.datasets[0].points[i].value --;
+	}
+	graph.update();
+}
+
+// Resizes the graphs when the window is resized. 
+window.onresize = function()
+{
+	for(var i =0; i < graphids.length; i++)
+	{
+		// updating
+		graphids[i].graph.update();
 	}
 }
 
@@ -33,7 +74,7 @@ var data1 =
 			strokeColor: 'rgba(0,0,0)',
 			highlightFill: 'rgba(209,105,214)',
 			highlightStroke: 'rgba(112,11,117)', 
-			data: [0,1,2,3,4,5,6,7,8,9,10]
+			data: [.5,1,1.5,2,2.5,3,3.5,4,4.5, 5]
 		}
 	]
 };
@@ -41,13 +82,13 @@ var data1 =
 // Pass in 2D context of each canas to initialize chart. 
 function initGraphs()
 {
-
 	for(var i =0; i < graphids.length; i++)
 	{
 		var ctx = document.getElementById(graphids[i].id).getContext('2d'); 
 
 		var myChart = new Chart(ctx).Line(data1, 
-			{ scaleShowGridLines: true, pointHitDetectionRadius: 10, datasetFill: true, responsive: true});
+			{ scaleOverride: true, scaleSteps: 10, scaleStepWidth: 1, 
+				scaleStartValue: 0, scalescaleShowGridLines: true, pointHitDetectionRadius: 10, datasetFill: true, responsive: true});
 		 
 		graphids[i].graph = myChart;
 
@@ -63,26 +104,21 @@ function initGraphs()
 			console.log( getGraphObj($(this).attr('id')));
 		});
 	}
-
 };
-
-window.onresize = function()
-{
-	for(var i =0; i < graphids.length; i++)
-	{
-		// updating
-		graphids[i].graph.update();
-	}
-}
-
-function updateGlobalChartConfigs()
-{
-
-};
-
 
 window.onload = function(){
 
 	console.log("initializing graphs");
 	initGraphs();
+
+	$('#plusHP').click(function()
+	{
+		incrementGraph( 'hpGraph');
+		console.log("incremented?");
+	});
+	$('#minusHP').click(function()
+	{
+		decrementGraph( 'hpGraph');
+		console.log("decremented?");
+	});
 };
