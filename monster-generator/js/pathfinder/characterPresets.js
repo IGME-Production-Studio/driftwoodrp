1060da825a0e43998111053e464e29fefbc1c20f
Gabrielle Bennett <gmb9280@rit.edu>
2015-02-19 23:05:15 -0500

    Started working on Pathfinder data files

---
+++ monster-generator/js/pathfinder/characterPresets.js
@@ -0,0 +1,42 @@
+Barbarian
+Bard
+Cleric
+Druid
+Fighter
+Monk
+Paladin
+Ranger
+Rogue
+Sorcerer
+Wizard
+Alchemist
+Cavalier
+Gunslinger
+Inquisitor
+Magus
+Oracle
+Summoner
+Witch
+Antipaladin
+Ninja
+Samurai
+Arcanist
+Bloodrager
+Brawler
+Hunter
+Investigator
+Shaman
+Skald
+Slayer
+Swashbuckler
+Warpriest
+Arcane Archer
+Arcane Trickster
+Assassin
+Dragon Disciple
+Duelist
+Eldritch Knight
+Loremaster
+Mystic Theurge
+Pathfinder Chronicler
+Shadowdancer
---
+++ monster-generator/js/pathfinder/statGrowth.js
@@ -0,0 +1,56 @@
+// Datafile for stat growth presets
+// BAB = Base Ability Bonus
+// "save" - saving throw
+var statgrowth = {}; 
+
+statgrowth.goodBAB = 
+[
+	1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1
+];
+
+statGrowth.avgBAB = 
+[
+	0, 1, 1, 1, 0,
+	1, 1, 1, 0, 1, 
+	1, 1, 0, 1, 1, 
+	1, 0, 1, 1, 1
+];
+
+statGrowth.poorBAB = 
+[
+	0, 1, 0, 1, 0,
+	1, 0, 1, 0, 1,
+	0, 1, 0, 1, 0,
+	1, 0, 1, 0, 1
+];
+
+statGrowth.goodSave = 
+[
+	2, 1, 0, 1, 0,
+	1, 0, 1, 0, 1,
+	0, 1, 0, 1, 0,
+	1, 0, 1, 0, 1
+];
+
+statGrowth.poorSave = 
+[
+	0, 0, 1, 0, 0,
+	1, 0, 0, 1, 0, 
+	0, 1, 0, 0, 1,
+	0, 0, 1, 0, 0
+];
+
+statGrowth.PgoodSave = 
+[
+	1, 0, 1, 0, 1,
+	0, 1, 0, 1, 0
+];
+
+statGrowth.PpoorSave = 
+[
+	0, 1, 0, 0, 1,
+	0, 0, 1, 0, 0
+];
