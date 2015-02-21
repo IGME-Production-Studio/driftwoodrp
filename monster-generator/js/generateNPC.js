4728085bee7417c7c16c8f3cf1ab419c27522992
Mario Chuman <mxc7819@g.rit.edu>
2015-02-21 16:20:35 -0500

    Fixed image uploader. It works now!

--- monster-generator/js/generateNPC.js
+++ monster-generator/js/generateNPC.js
@@ -73,15 +73,19 @@ function checkForChange()
 		console.log("image changed");
 		previewFile();
 	};*/
+	
+	imageManipulation();
 }
-
+/*
+//file to upload image temporarily on page
+//currently not working
 function previewFile()
 {
 	var preview = document.querySelector('tempPicture');
 	var file = document.querySelector('input[type=file]').files[0];
 	var reader = new FileReader();
-	console.log(preview.src);
-	console.log(reader.result);
+	//reader doesn't load... why?
+	console.log(reader);
 	reader.onloaded = function()
 	{
 		console.log(preview.src);
@@ -97,6 +101,26 @@ function previewFile()
 	{
 		preview.src = "";
 	}
+}*/
+
+function imageHandler(e2)
+{
+	var store = document.getElementById('imgstore');
+	store.innerHTML='<img src="' + e2.target.result + '" height = 187 width = 280>';
+}
+
+function loadimage(e1)
+{
+	var filename = e1.target.files[0];
+	var fr = new FileReader();
+	fr.onload = imageHandler;
+	fr.readAsDataURL(filename);
+}
+
+function imageManipulation()
+{
+	var y = document.getElementById("getimage");
+	y.addEventListener('change',loadimage, false);
 }
 
 //generates the NPC
