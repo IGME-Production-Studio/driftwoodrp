a2a4053a50ea5da2282665c3688c6ae087c1a797
unknown <gmb9280@OH1375-26.main.ad.rit.edu>
2015-03-12 17:09:56 -0400

    Added sample creature with appropriate structure of data.

--- monster-generator/js/creatureTemplate.js
+++ monster-generator/js/creatureTemplate.js
@@ -17,3 +17,24 @@ var creatureTemplate =
 	"skills": [],
 	"speed": { "speed": "", "baseSpeed": 0, "flySpeed": "", "maneuverability": "", "climbSpeed": 0, "swimSpeed": 0, "burrowSpeed": 0, "speedSpecial": "", "speedLand": 0, "fly": 0, "climb": 0, "burrow": 0, "swim": 0}
 };
+
+// Albino Cave Spider
+var sampleCreature = 
+{
+	"name": "Albino Cave Spider",
+	"alignment": "N",
+	"drops": { "xp": 200, "treasure": "Standard", "gear": "" },
+	"classes": { "class1": {"name": "", "levels": 0 }, "class2": {"name": "", "levels": 0} },
+	"classifications": {"type": "Vermin", "subtype": [], "group": "Spider"},
+	"features": {"languages": [], "sq": "", "environment": "Underground", "organization": "Solitary or nest (2-20)"},
+	"isCharacter": false, 
+	"isCompanion": false,
+	"savingThrows": {"fort": 0, "ref": 0, "wil": 0 }, 
+	"combatInfo": {"cr": 0, "melee": "", "ranged": "" , "ac": 0, "ac_touch": 0, "ac_flatFooted": 0 },
+	"spatial": {"size": "", "space": 0, "reach": 0}, 
+	"abilityScores": {"str": 0, "dex": 0, "con": 0, "int": 0, "wis": 0, "cha": 0},
+	"hitPoints": {"hitDie": "", "hp": 0}, 
+	"feats": [], 
+	"skills": [],
+	"speed": { "speed": "", "baseSpeed": 0, "flySpeed": "", "maneuverability": "", "climbSpeed": 0, "swimSpeed": 0, "burrowSpeed": 0, "speedSpecial": "", "speedLand": 0, "fly": 0, "climb": 0, "burrow": 0, "swim": 0}
+};
