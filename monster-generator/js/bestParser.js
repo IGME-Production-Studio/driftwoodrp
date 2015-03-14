8f17c46b396575f07de48832bbe1976b2f2e5588
Gabrielle Bennett <gmb9280@rit.edu>
2015-03-14 15:03:30 -0400

    Added class information to parser

--- monster-generator/js/bestParser.js
+++ monster-generator/js/bestParser.js
@@ -2,19 +2,26 @@
 
 function doTheParse()
 {
-	var mainText; 
+	var mainText = "[";
 	for(var i =0; i< bestiary.length; i++)
 	{
 		var subText = "{\"name\": \""+ bestiary[i].Name + "\","; 
 		subText += "\"alignment\": \"" +bestiary[i].Alignment + "\",";
 
-		subText += "\"drops\":{ \"xp\":\"" + bestiary[i].XP + "\",\"treasure\": \""+  bestiary[i].Treasure + "\", \"gear\": \"" + bestiary[i].Gear +"\"}";
+		subText += "\"drops\":{ \"xp\":\"" + bestiary[i].XP + "\",\"treasure\": \""+  bestiary[i].Treasure + "\", \"gear\": \"" + bestiary[i].Gear +"\"},";
 
 		// classes
-		//subText += "'classes': {"
-		subText += "},"; 
+		subText += "\"classes\": { \"class1\": { \"name\": \"" + 
+			bestiary[i].Class1 + "\", \"levels\": \"" 
+			+ bestiary[i].Class1_Lvl + "\"}, \"class2\": { \"name\": \"" 
+			+ bestiary[i].Class2 + "\", \"levels\": \"" 
+			+ bestiary[i].Class2_Lvl + "\"}}";
+
+		subText += "}";
+		if(i < bestiary.length-1) subText += ","
 		mainText += subText; 
 	}
 
+	mainText+="]";
 	$('#main').html(mainText);
 }
