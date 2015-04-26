b43ffdb1ace420daf0f1b5b0ac6b3d926834e40c
unknown <mxc7819@NM2550-09.main.ad.rit.edu>
2015-04-26 14:26:17 -0400

    Updated to fit with coding standards.

--- update/public/monster-generator/js/generateNPC.js
+++ update/public/monster-generator/js/generateNPC.js
@@ -1,45 +1,54 @@
+//*************************************************************
+//
+// Filename: generateNPC.js
+//	
+// Authors:
+//  Mario Chuman	mxc7819@g.rit.edu
+//
+//*************************************************************
+
 var name, challenge, type, size, alignment, movement;
 
 window.onload = function(){
-	name = "undefined";
-	challenge = "undefined";
-	type = "undefined";
-	size = "Tiny";
-	alignment = "Unaligned";
-	movement = "Burrow";
-	
-	console.log("Name: " + name + "\nChallenge Rating: " + challenge + "\nType: " + type + "\nSize Rating: " + size + "\nAlignment: " + alignment + "\nMovement Type: " + movement);
-	
-	checkForChange();
-	generate();
+  name = "undefined";
+  challenge = "undefined";
+  type = "undefined";
+  size = "Tiny";
+  alignment = "Unaligned";
+  movement = "Burrow";
+
+  console.log("Name: " + name + "\nChallenge Rating: " + challenge + "\nType: " + type + "\nSize Rating: " + size + "\nAlignment: " + alignment + "\nMovement Type: " + movement);
+
+  checkForChange();
+  generate();
 };
 
 function checkForChange()
 {
-	document.getElementById("sizeStatInput").onchange = function()
-	{
-		size = document.getElementById("sizeStatInput").value;
-		console.log("New Size is " + size);
-	};
-	
-	
-	document.getElementById("alignmentStatInput").onchange = function()
-	{
-		alignment = document.getElementById("alignmentStatInput").value;
-		console.log("New Alignment is " + alignment);
-	};
-	
-	document.getElementById("movementStatInput").onchange = function()
-	{
-		movement = document.getElementById("movementStatInput").value;
-		console.log("New Movement is " + movement);
-	};
+  document.getElementById("sizeStatInput").onchange = function()
+  {
+    size = document.getElementById("sizeStatInput").value;
+    console.log("New Size is " + size);
+  };
+
+
+  document.getElementById("alignmentStatInput").onchange = function()
+  {
+    alignment = document.getElementById("alignmentStatInput").value;
+    console.log("New Alignment is " + alignment);
+  };
+
+  document.getElementById("movementStatInput").onchange = function()
+  {
+    movement = document.getElementById("movementStatInput").value;
+    console.log("New Movement is " + movement);
+  };
 }
 
 function generate()
 {
-	document.getElementById("generateButton").onclick = function()
-	{
-		console.log("Name: " + name + "\nChallenge Rating: " + challenge + "\nType: " + type + "\nSize Rating: " + size + "\nAlignment: " + alignment + "\nMovement Type: " + movement);
-	};
+  document.getElementById("generateButton").onclick = function()
+  {
+    console.log("Name: " + name + "\nChallenge Rating: " + challenge + "\nType: " + type + "\nSize Rating: " + size + "\nAlignment: " + alignment + "\nMovement Type: " + movement);
+  };
 }
