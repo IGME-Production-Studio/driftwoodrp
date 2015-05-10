ad0132cc8bc875723ca43bebc1282b4adb21f1fe
Mario Chuman <mxc7819@g.rit.edu>
2015-05-10 14:50:48 -0400

    Edited router.js to meet coding standards.

--- src/router.js
+++ src/router.js
@@ -29,9 +29,9 @@ var router = function(app, controllers, mid) {
   app.use(mid.requests);
 	app.use(mid.responses);
 
-	app.get('/', mid.requiresNoAuth, controllers.Players.loginPage);
-	app.get('/login', mid.requiresNoAuth, controllers.Players.loginPage);
-	app.post('/login', mid.requiresNoAuth, controllers.Players.login);
+  app.get('/', mid.requiresNoAuth, controllers.Players.loginPage);
+  app.get('/login', mid.requiresNoAuth, controllers.Players.loginPage);
+  app.post('/login', mid.requiresNoAuth, controllers.Players.login);
   app.get('/logout', controllers.Players.logout);
   app.post('/logout', controllers.Players.logout);
   app.post('/acceptTerms', controllers.Players.acceptTerms);
@@ -39,21 +39,21 @@ var router = function(app, controllers, mid) {
   app.get('/terms', controllers.Players.termsConditions);
   app.get('/createTerms', controllers.Players.registerTermsPage);
   app.get('/createAccount', mid.requiresNoAuth, controllers.Players.registerPage);
-	app.post('/createAccount', mid.requiresNoAuth, controllers.Players.createAccount);
+  app.post('/createAccount', mid.requiresNoAuth, controllers.Players.createAccount);
   app.get('/resetPassword', controllers.Players.resetPasswordPage);
   app.post('/resetPassword',  controllers.Players.resetPassword);
-	app.get('/changePassword', mid.requiresAuth, controllers.Players.changePasswordPage);	
-	app.post('/changePassword', mid.requiresAuth, controllers.Players.changePassword);	
+  app.get('/changePassword', mid.requiresAuth, controllers.Players.changePasswordPage);	
+  app.post('/changePassword', mid.requiresAuth, controllers.Players.changePassword);	
   app.get('/grid.svg', mid.requiresAuth, controllers.Sessions.gridPage);
-	app.get('/joinGame/:player', mid.requiresAuth, mid.requiresTosAgreement, mid.requiresOwnership, controllers.Sessions.joinSessionPage);
-	app.post('/createGame/:player', mid.requiresAuth, mid.requiresOwnership, controllers.Sessions.createSession);
-	app.get('/game/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.loadSession);
-	app.post('/addPlayer/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.addPlayer);
-	app.post('/removePlayer/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.removePlayer);
+  app.get('/joinGame/:player', mid.requiresAuth, mid.requiresTosAgreement, mid.requiresOwnership, controllers.Sessions.joinSessionPage);
+  app.post('/createGame/:player', mid.requiresAuth, mid.requiresOwnership, controllers.Sessions.createSession);
+  app.get('/game/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.loadSession);
+  app.post('/addPlayer/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.addPlayer);
+  app.post('/removePlayer/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.removePlayer);
   app.post('/addGM/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.addGM);
-	app.post('/removeGM/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.removeGM);
-	app.post('/uploadToken/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.uploadToken);
-	app.post('/removeToken/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.removeToken);
+  app.post('/removeGM/:player/:gameName', mid.requiresAuth, mid.requiresOwnership, mid.attachGame, controllers.Sessions.removeGM);
+  app.post('/uploadToken/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.uploadToken);
+  app.post('/removeToken/:player/:gameName', mid.requiresAuth, mid.attachGame, controllers.Sessions.removeToken);
   //app.get('*', mid.requiresNoAuth, controllers.Players.loginPage);
 };
 
