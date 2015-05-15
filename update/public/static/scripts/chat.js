35d3e678c82d9afaad69949fce86128fafc485c5
Gabrielle Bennett <gmb9280@rit.edu>
2015-05-15 08:21:38 -0400

    Chat (without server) test is working decently; there is a little bit of wonky CSS that needs help in chat. Other than that, restyled right hand panel a bit to get things working correctly. Reworked most of HTML in the page.

--- update/public/static/scripts/chat.js
+++ update/public/static/scripts/chat.js
@@ -9,13 +9,45 @@ function chat() {
 }
 
 chat.prototype.initialize = function() {
+
+	$('#chatinput-send').click(function()
+	{
+		console.log("CLICKED");
+		var msg = $('#chatinput-input').val();
+		console.log("msg: " + msg);
+		sendMessageTest(msg, '1234ABC', CallerID);
+	});
+	$('#chatinput-input').keydown(function(e)
+	{
+		if(e.keyCode == '13')
+		{
+			console.log("enter pressed");
+			var msg = $('#chatinput-input').val();
+			console.log("msg: " + msg);
+			sendMessageTest(msg, '1234ABC', CallerID);
+		}
+	})
+
   console.log("Chat Initialized");
 }
 
 chat.prototype.sendMessage = function(msg){
 	 Socket.emit('sendMessage', msg, RoomID, CallerID);
+	 var newp = document.createElement('p'); 
+	 $('#newp').addClass('chatMsg').html('[' + CallerID + ']' + ': ' + msg); 
+	 $('#chatwindow').append(newp);
 }
-
+function sendMessageTest(msg, roomID,  CallerID)
+{
+	console.log("chat test");
+	console.log(msg);
+	 var newp = document.createElement('p'); 
+	 var h = '[' + CallerID + ']' + ': ' + msg;
+	 $(newp).addClass('chatMsg');
+	 $(newp).html(h); 
+	 $('#chatwindow').append(newp);
+	 $('#chatinput-input').val("");
+}
 
 chat.prototype.addEventListeners = function()
 {
